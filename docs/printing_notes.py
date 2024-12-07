ca4e2cbef43408baee9647b97422125d268e5111
Kelly Roman <kelly.roman@outlook.com>
2024-12-07 11:37:27 -0500

    printing notes

---
+++ docs/printing_notes.py
@@ -0,0 +1,21 @@
+'''
+Insert Card Printing
+
+Templates:
+3.5 x 2 
+One page (typically front) is flipped 180 degrees to align printer error
+Print templates are 3 cards per sheet (4x6). Use wrapper.insert_print()
+Note, template will overlap cards to slightly narrower than 2" to account for printer margins
+
+Use Adobe** for printing, with settings:
+- Document size is correct (4x6)
+- Borderless
+- Photo Paper Glossy
+- High Quality
+- Actual Size
+
+On Printer:
+- Document size is correct (4x6)
+- Premium Photo Glossy
+
+'''
